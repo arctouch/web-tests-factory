8934f99eaacf3e5f4e12b535ab098f67492940f7
arctouch-leticiacoelho <leticia.coelho@arctouch.com>
2021-09-21 17:06:19 -0300

    Solve bug on echo

--- testsFactory.sh
+++ testsFactory.sh
@@ -36,10 +36,10 @@ describe ('Verify testingMethod using testingMock to validate the scenario ABC/'
 
 NEXT_MOCK="const testingMock= \"Scenario A is success\";"
 
-echo -e  "\nHello, Welcome to ${ORANGE}Tests Factory${NC}! \nWith this script you will have a new default folder to implement yout unit tests \n"
-echo -e  "${ORANGE}Please, select the Tests Framework you should use on this project: ${NC}\n"
+echo "\nHello, Welcome to ${ORANGE}Tests Factory${NC}! \nWith this script you will have a new default folder to implement yout unit tests \n"
+echo "${ORANGE}Please, select the Tests Framework you should use on this project: ${NC}\n"
 
-echo -e -n "1. Jest\n\n${RED}Select Framework >>${NC} "
+echo "1. Jest\n\n${RED}Select Framework >>${NC} "
 read -r FRAMEWORK_REFERENCE_NUMBER
 
 if FRAMEWORK_REFERENCE_NUMBER=1; then
@@ -53,16 +53,15 @@ if FRAMEWORK_REFERENCE_NUMBER=1; then
 fi
 
 ## Insert new folder and tests
-  echo -e "${ORANGE}#${NC} ${FRAMEWORK_NAME} Framework"
+  echo "${ORANGE}#${NC} ${FRAMEWORK_NAME} Framework"
   mkdir ${TEST_FOLDER}
-  echo -e "${ORANGE}##${NC} Create new ${TEST_FOLDER} folder"
+  echo "${ORANGE}##${NC} Create new ${TEST_FOLDER} folder"
   mkdir ${TEST_MOCK_FOLDER}
-  echo -e "${ORANGE}###${NC} Create new ${TEST_MOCK_FOLDER} folder"
+  echo "${ORANGE}###${NC} Create new ${TEST_MOCK_FOLDER} folder"
   echo "${FRAMEWORK_TEST}" >> ${TEST_FILE}
-  echo -e "${ORANGE}####${NC} Insert default test on tests folder"
+  echo "${ORANGE}####${NC} Insert default test on tests folder"
   echo "${FRAMEWORK_MOCK}" >> ${MOCK_FILE}
-  echo -e "${ORANGE}#####${NC} Insert default mock on tests/mocks folder"
-  echo -e "\n${GREEN}##############################${NC}"
-  echo -e "${GREEN} Successfully created! ${NC}"
-  echo -e "${GREEN}##############################${NC}"
-
+  echo "${ORANGE}#####${NC} Insert default mock on tests/mocks folder"
+  echo "\n${GREEN}##############################${NC}"
+  echo "${GREEN} Successfully created! ${NC}"
+  echo "${GREEN}##############################${NC}"
