2096b88e929174fb8f59f20d2a74e3df50c3ab84
Igor Paranhos <igor.paranhos@arctouch.com>
2021-10-07 21:15:32 -0300

    Add xunit config to testsFactory.sh

--- testsFactory.sh
+++ testsFactory.sh
@@ -133,6 +133,59 @@ func TestMethod() (string) {
 }
 "
 
+XUNIT_TEST="using System;
+using Xunit;
+
+namespace Tests
+{
+    public class ClassNameTest
+    {
+        [Fact]
+        public void ShouldReturnSuccessMessage()
+        {
+            var mock = new ClassNameMock();
+            Assert.Equal(\"Scenario A is a success\", mock.TestMethod());
+        }
+    }
+}
+"
+
+XUNIT_MOCK="namespace Tests
+{
+    public class ClassNameMock
+    {
+        public string TestMethod()
+        {
+            return \"Scenario A is a success\";
+        }
+    }
+}
+"
+
+XUNIT_EXAMPLE="<Project Sdk=\"Microsoft.NET.Sdk\">
+
+    <PropertyGroup>
+        <TargetFrameworks>net5.0</TargetFrameworks>
+
+        <IsPackable>false</IsPackable>
+    </PropertyGroup>
+
+    <ItemGroup>
+        <PackageReference Include=\"Microsoft.NET.Test.Sdk\" Version=\"16.9.4\" />
+        <PackageReference Include=\"xunit\" Version=\"2.4.1\" />
+        <PackageReference Include=\"xunit.runner.visualstudio\" Version=\"2.4.3\">
+            <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
+            <PrivateAssets>all</PrivateAssets>
+        </PackageReference>
+        <PackageReference Include=\"coverlet.collector\" Version=\"3.0.2\">
+            <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
+            <PrivateAssets>all</PrivateAssets>
+        </PackageReference>
+    </ItemGroup>
+
+</Project>
+"
+
 echo -e \
 """
 Hello, Welcome to ${ORANGE}Tests Factory${NC}!
@@ -145,6 +198,7 @@ echo -e \
 2. PyUnit
 3. Rspec
 4. Go
+5. xUnit
 """
 
 echo -ne "${RED}Select Framework >>${NC} "
@@ -204,6 +258,19 @@ elif [[ $FRAMEWORK_REFERENCE_NUMBER == "4" ]]; then
   FRAMEWORK_TEST=${GO_TEST}
 
   INIT_CONTENT=${GO_EXAMPLE}
+elif [[ $FRAMEWORK_REFERENCE_NUMBER == "5" ]]; then
+  FRAMEWORK_NAME="xunit"
+
+  TEST_FOLDER="Tests"
+  TEST_MOCK_FOLDER="Tests/mocks"
+
+  TEST_FILE="Tests/ClassNameTest.cs"
+  MOCK_FILE="Tests/mocks/ClassNameMock.cs"
+  INIT_FILE="Tests/Tests.csproj"
+
+  FRAMEWORK_MOCK=${XUNIT_MOCK}
+  FRAMEWORK_TEST=${XUNIT_TEST}
+  INIT_CONTENT=${XUNIT_EXAMPLE}
 fi
 
 ## Insert new folder and tests
