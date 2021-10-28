c4f94a633aa9a038f50bd906dd00424829b2693a
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2021-10-28 13:16:44 -0300

    Add dynamic methods for rspec

--- bin/templates/rspec.js
+++ bin/templates/rspec.js
@@ -3,41 +3,62 @@ const {
   snakeCase,
 } = require('change-case');
 
-module.exports = (filename, methods) => [
-  {
-    name: `spec/${filename}_spec.rb`,
-    template: `require "./${snakeCase(filename)}.rb"
+const defaultValues = {
+  string: '\'Scenario A is success\'',
+  number: 0,
+  bool: 'false',
+};
+
+module.exports = (filename, methods) => {
+  // eslint-disable-next-line no-return-assign
+  const formattedMethod = methods.reduce((acc, elm) => ({
+    methods: acc.methods.concat({
+      name: snakeCase(elm.name),
+      type: defaultValues[elm.type] !== undefined ? elm.type : 'string',
+      value: defaultValues[elm.type] !== undefined ? defaultValues[elm.type] : defaultValues.string,
+    }),
+    names: acc.names += `\n  ${snakeCase(elm.name)},`,
+    mocks: acc.mocks += `\n  ${snakeCase(elm.name)}Mock,`,
+  }), {
+    names: '',
+    mocks: '',
+    methods: [],
+  });
+
+  return [
+    {
+      name: `spec/${filename}_spec.rb`,
+      template: `require "./${snakeCase(filename)}.rb"
 require "./mocks/${snakeCase(filename)}_mock.rb"
 
 describe ${pascalCase(filename)} do
-    describe "#test_method" do
-
-        before do
-            @test_class = ${pascalCase(filename)}.new()
-            @mocks = ${pascalCase(filename)}Mock.new()
-        end
-
-
-        it "puts the ordered book in customer's order history" do
-            expect(@test_class.test_method).to eq(@mocks.mock_method_01_result)
-        end
+  ${formattedMethod.methods.map(({ name, type }) => `describe "#${name}" do
+    before do
+      @test_class = ${pascalCase(filename)}.new()
+      @mocks = ${pascalCase(filename)}Mock.new()
     end
-end`,
-  },
-  {
-    name: `mocks/${filename}_mock.rb`,
-    template: `class ${pascalCase(filename)}Mock
-    def mock_method_01_result
-        "Scenario A is success"
+
+    it "should return a value of type ${type}" do
+      expect(@test_class.${name}).to eq(@mocks.${name})
     end
+  end`).join('\n  ')}
 end`,
-  },
-  {
-    name: `${filename}.rb`,
-    template: `class ${pascalCase(filename)}
-    def test_method
-        "Scenario A is success"
-    end
+    },
+    {
+      name: `mocks/${filename}_mock.rb`,
+      template: `class ${pascalCase(filename)}Mock
+  ${formattedMethod.methods.map(({ name, value }) => `def ${name}
+    return ${value}
+  end`).join('\n  ')}
 end`,
-  },
-];
+    },
+    {
+      name: `${filename}.rb`,
+      template: `class ${pascalCase(filename)}
+    ${formattedMethod.methods.map(({ name, value }) => `def ${name}
+    return ${value}
+  end`).join('\n  ')}
+end`,
+    },
+  ];
+};
